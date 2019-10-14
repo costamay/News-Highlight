5b91bd38d2f357a14f00b337b42dddacd8d9541a
Titus Ouko <titusouko@gmail.com>
2019-10-15 00:39:39 +0300

    import function to search and creeate route for serch by name to be dynamic

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,6 @@
 from flask import render_template,request,redirect,url_for
 from . import main
-from ..requests import get_sources, get_articles
+from ..requests import get_sources, get_articles,search_news
 
 
 
@@ -19,7 +19,11 @@ def index():
     news_source = get_sources()
     
     title = 'Home - Welcome to The best News  Highlight Website Online'
-    return render_template('index.html', title = title,news_source = news_source )
+    search_news = request.args.get('news_query')
+    if search_news:
+        return redirect(url_for('main.search', name=search_news))
+    else:
+        return render_template('index.html', title = title,news_source = news_source )
 
 @main.route('/news/<id>')
 def news(id):
@@ -27,4 +31,14 @@ def news(id):
     print(article)
     return render_template('article.html',article = article)
 
- 
+@main.route('/search/<name>')
+def search(name):
+    '''
+    View function to display the seach results
+    '''
+
+    movie_name_list = name.split(" ")
+    movie_name_format = "+".join(movie_name_list)
+    searched_movies = search_news(movie_name_format)
+    title = f'search results for {name}'
+    return render_template('search.html', movies = searched_movies, title=title) 
